13fd2f61bcb94a665f5d51f5643f4addc69bc0e0
Dan Gilbert <danielcgilbert@gmail.com>
2016-07-31 11:37:51 -0500

    Mocking out data w/default prop values for now.

--- src/components/recently-viewed-routes.js
+++ src/components/recently-viewed-routes.js
@@ -1,32 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes, } from 'react';
+
 import {
   Text,
   TouchableOpacity,
   ScrollView,
   View,
 } from 'react-native';
-import UserActions from '../../src/actions/user.js';
+
+import Styles from '../styles/styles.ios';
+import UserActions from '../actions/user.js';
 
 export default class RecentlyViewedRoutes extends React.Component {
+
+  static propTypes = {
+    routes: PropTypes.array.isRequired,
+  };
+
+  static defaultProps = {
+    routes: [
+      { rt: '8', rtnm: 'Halsted' },
+      { rt: '9', rtnm: 'Balsted' },
+      { rt: '10', rtnm: 'Galsted' },
+      { rt: '11', rtnm: 'Jalsted' },
+      { rt: '12', rtnm: 'Kalsted' },
+      { rt: '13', rtnm: 'Lalsted' },
+      { rt: '14', rtnm: 'Malsted' },
+      { rt: '15', rtnm: 'Nalsted' },
+      { rt: '16', rtnm: 'Oalsted' },
+    ],
+  };
+
+
   render() {
-    let recentRoutes = this.props.routes || [];
-    console.warn(recentRoutes, 'are the recent routes')
+    let { routes } = this.props;
+
     return (
       <View style={Styles.recentlyViewedRoutes}>
-      <ScrollView horizontal={true} scrollsToTop={false} showsHorizontalScrollIndicator={false}>
-        { recentRoutes.map((route, i) =>
-          <TouchableOpacity
-            onPress={() =>
-                        UserActions.handleRouteSelection(route)}
-            key={i}
-            style={Styles.recentlyViewedRoute}>
-            <Text allowFontScaling={false} style={Styles.recentlyViewedRouteNumber}>
-              {route.rt}
-            </Text>
-          </TouchableOpacity>
-          )}
-      </ScrollView>
-    </View>
+        <ScrollView horizontal={true} scrollsToTop={false} showsHorizontalScrollIndicator={false}>
+          { routes.map((route, i) =>
+            <TouchableOpacity
+              onPress={() => UserActions.handleRouteSelection(route)}
+              key={i}
+              style={Styles.recentlyViewedRoute}>
+              <Text allowFontScaling={false} style={Styles.recentlyViewedRouteNumber}>
+                {route.rt}
+              </Text>
+            </TouchableOpacity>
+            )}
+        </ScrollView>
+      </View>
     );
   }
+
 }
